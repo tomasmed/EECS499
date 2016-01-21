075cf82672cd6726efba3fd5a261b17c47602845
Tomas <tomamed@umich.edu>
2016-01-20 19:11:08 -0500

    first commint pajamam jam

---
+++ HelloWorld/Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//print ("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("Hello World!");
+	}
+}
