4f83a17d301c58a470206443330eb7cc6cca5dd9
medstone <medstone@umich.edu>
2016-01-23 04:58:53 -0500

    Swinging Added
    Got the basic function for smooth swinging implemented. I need to do
    some work on the movement to get them to work properly and make the
    swing generalized for the all situations of holding and releasing. Also,
    I had a bunch of issues running the movement stuff due to nonexistant
    axis. Will look more into this tomorrow.

---
+++ ClimbingGame/Assets/ropeManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ropeManager : MonoBehaviour {
+    public GameObject member1;
+    public GameObject member2;
+    LineRenderer rope;
+
+	// Use this for initialization
+	void Start () {
+        rope = member1.GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rope.SetPosition(0, member1.transform.position);
+        rope.SetPosition(1, member2.transform.position);
+	}
+}
