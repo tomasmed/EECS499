3759eea3ede37e060747ab40b58a335b1a52aac9
medstone <medstone@umich.edu>
2016-01-24 18:11:27 -0500

    New Controller and Swinging
    Wrote my own controller that works better for allowing both to swing and
    getting the same behavior. Going to work on making some fun areas and
    mechanics to go along with it.

---
+++ ClimbingGame/Assets/_Scripts/nonCharacterControllerMovement.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nonCharacterControllerMovement : MonoBehaviour {
+    public float speed;
+    public Rigidbody bod;
+    public enum controles { WASD = 0, ARROW };
+    public controles scheme = controles.WASD;
+    public bool freeSwing;
+    public Vector3 left;
+    public Vector3 right;
+    public bool leftCheck;
+    public bool rightCheck;
+    public bool latched;
+
+    // Use this for initialization
+    void Start () {
+        bod = gameObject.GetComponent<Rigidbody>();
+	}
+
+    void Update()
+    {
+        left = gameObject.transform.position + new Vector3(-(gameObject.transform.lossyScale.x / 2), gameObject.transform.lossyScale.y / 2, gameObject.transform.lossyScale.z / 2);
+        right = gameObject.transform.position + gameObject.transform.lossyScale / 2;
+        switch (scheme)
+        {
+            case controles.ARROW:
+                if ((Input.GetKey(KeyCode.Home) || Input.GetKey(KeyCode.LeftShift)) && (Input.GetKey(KeyCode.PageUp) || Input.GetKey(KeyCode.Return)))
+                {
+                    freeSwing = true;
+                    bod.useGravity = true;
+                    latched = false;
+                }
+                else if ((!Input.GetKey(KeyCode.Home) && !Input.GetKey(KeyCode.LeftShift)) || (!Input.GetKey(KeyCode.PageUp) && !Input.GetKey(KeyCode.Return)))
+                {
+                    freeSwing = false;
+                }
+                break;
+            case controles.WASD:
+                if ((Input.GetKey(KeyCode.Q)) && (Input.GetKey(KeyCode.E)))
+                {
+                    freeSwing = true;
+                    bod.useGravity = true;
+                    latched = false;
+                }
+                else if (!(Input.GetKey(KeyCode.Q)) || !(Input.GetKey(KeyCode.E)))
+                {
+                    freeSwing = false;
+                }
+                break;
+        }
+        leftCheck = Physics.Raycast(left, gameObject.transform.forward, 1f);
+        rightCheck = Physics.Raycast(right, gameObject.transform.forward, 1f);
+        if (!freeSwing && (leftCheck || rightCheck))
+        {
+            bod.useGravity = false;
+            latched = true;
+            bod.velocity = Vector3.zero;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Vector3 change = Vector3.zero;
+        if (!latched) return;
+        switch (scheme)
+        {
+            case controles.WASD:
+                if (Input.GetKey(KeyCode.W))
+                {
+                    change.y += 1;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    change.y -= 1;
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    change.x -= 1;
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    change.x += 1;
+                }
+                break;
+            case controles.ARROW:
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    change.y += 1;
+                }
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    change.y -= 1;
+                }
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    change.x -= 1;
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    change.x += 1;
+                }
+                break;
+
+        }
+        change = change.normalized * speed * Time.fixedDeltaTime;
+        bod.transform.position += change;
+    }
+}
--- ClimbingGame/Assets/_Scripts/swingManager.cs
+++ ClimbingGame/Assets/_Scripts/swingManager.cs
@@ -2,25 +2,64 @@
 using System.Collections;
 
 public class swingManager : MonoBehaviour {
-    public GameObject other;
+    public GameObject player1;
+    public GameObject player2;
+    public nonCharacterControllerMovement p1;
+    public nonCharacterControllerMovement p2;
     public float length;
-    Rigidbody bod;
+    Rigidbody bod1;
+    Rigidbody bod2;
 
 	// Use this for initialization
 	void Start () {
-        bod = this.gameObject.GetComponent<Rigidbody>();
-	}
+        bod1 = player1.gameObject.GetComponent<Rigidbody>();
+        bod2 = player2.gameObject.GetComponent<Rigidbody>();
+        p1 = player1.GetComponent<nonCharacterControllerMovement>();
+        p2 = player2.GetComponent<nonCharacterControllerMovement>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        Vector3 dist = this.transform.position - other.transform.position;
-        if(dist.magnitude > length)
+	void FixedUpdate () {
+        if (p1.latched && !p2.latched)
         {
-            this.gameObject.transform.position = other.gameObject.transform.position + (dist.normalized * length);
-            Vector3 vel = bod.velocity;
-            float mag = vel.magnitude;
-            vel = vel.normalized - (dist.normalized * Vector3.Dot(dist.normalized, vel.normalized));
-            bod.velocity = vel * mag;
+            Vector3 dist = player2.transform.position - player1.transform.position;
+            if (dist.magnitude > length)
+            {
+                player2.gameObject.transform.position = player1.transform.position + (dist.normalized * ((length)));
+                Vector3 vel = bod2.velocity;
+                float mag = vel.magnitude;
+                vel = vel.normalized - ((dist.normalized * Vector3.Dot(dist.normalized, vel.normalized)));
+                bod2.velocity = vel * mag;
+            }
+        }
+        else if (!p1.latched && p2.latched)
+        {
+            Vector3 dist = player1.transform.position - player2.transform.position;
+            if (dist.magnitude > length)
+            {
+                player1.gameObject.transform.position = player2.transform.position + (dist.normalized * ((length)));
+                Vector3 vel = bod1.velocity;
+                float mag = vel.magnitude;
+                vel = vel.normalized - ((dist.normalized * Vector3.Dot(dist.normalized, vel.normalized)));
+                bod1.velocity = vel * mag;
+            }
+        }
+        else
+        {
+            Vector3 dist = player2.transform.position - player1.transform.position;
+            if (dist.magnitude > length)
+            {
+                player2.gameObject.transform.position += (dist.normalized * ((length - dist.magnitude) / 2));
+                player1.gameObject.transform.position -= (dist.normalized * ((length - dist.magnitude) / 2));
+                Vector3 vel = bod1.velocity;
+                float mag = vel.magnitude;
+                vel = vel.normalized - ((dist.normalized * Vector3.Dot(dist.normalized, vel.normalized)) / 2);
+                bod1.velocity = vel * mag;
+                vel = bod2.velocity;
+                mag = vel.magnitude;
+                vel = vel.normalized - ((dist.normalized * Vector3.Dot(dist.normalized, vel.normalized)) / 2);
+                bod1.velocity = vel * mag;
+            }
         }
     }
 }
